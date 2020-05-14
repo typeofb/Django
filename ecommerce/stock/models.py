cd9f1269429074c7f3ce16ad0f296d385d8a9f0c
typeofb@gmail.com <jordan12>
2020-05-14 17:14:58 +0900

    python manage.py inspectdb

--- ecommerce/stock/models.py
+++ ecommerce/stock/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Stock(models.Model):
+    category = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    cnt = models.IntegerField(blank=True, null=True)
+    img = models.CharField(max_length=255, blank=True, null=True)
+    createdat = models.DateTimeField(db_column='createdAt', blank=True, null=True)  # Field name made lowercase.
+    updatedat = models.DateTimeField(db_column='updatedAt', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'stock'
