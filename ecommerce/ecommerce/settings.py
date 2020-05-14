1ede3111a739ad04c15275baa6faabf7c84f21ef
typeofb@gmail.com <jordan12>
2020-05-14 17:12:29 +0900

    add stock

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prac_dev',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES"'
+        }
     }
 }
 
