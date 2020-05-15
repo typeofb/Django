ce24737f6137a9e3cc772c682c463f6a44e3d924
typeofb@gmail.com <jordan12>
2020-05-15 09:47:43 +0900

    query, views 추가

--- ecommerce/stock/views.py
+++ ecommerce/stock/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 import logging
+from .models import Stock
 
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 def stock_list(request):
     logger.debug('request stock_list')
-    return render(request, 'stock/stock_list.html', {})
+
+    stocks = Stock.objects.raw("SELECT id, category, name, price, cnt, img FROM stock")
+    logger.debug(list(stocks))
+
+    return render(request, 'stock/stock_list.html', {'stocks': stocks})
